9d7b9a147c01ea69ff0f77443f817c8012f5ae63
Vasil Voloshin <voloshin017@gmail.com>
2024-11-16 13:50:45 +0200

    fix config

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const config = require('config');
+const dotenv = require('dotenv').config();
 const authRoutes = require('./routes/auth.routes');
 const recipeRoutes = require('./routes/recipe.routes');
+
+const url = dotenv.parsed.ATLAS_URL || '';
+const port = dotenv.parsed.PORT || 3000;
 
 const app = express();
 
@@ -14,7 +17,7 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' })); // Обмеження, якщо потрібно
 
 // Підключення до MongoDB
-mongoose.connect(config.get('mongoUrl'))
+mongoose.connect(url)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
@@ -27,5 +30,5 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
-const PORT = config.get('port') || 3000;
+const PORT = port || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
