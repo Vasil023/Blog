8af406bff94a136d8098aefbb70bf8a9547e274e
Vasil Voloshin <voloshin017@gmail.com>
2024-11-16 17:17:03 +0200

    fix cors

--- server/app.js
+++ server/app.js
@@ -10,6 +10,14 @@ const recipeRoutes = require('./routes/recipe.routes');
 const url = process.env.MONGODB_URI || 'mongodb+srv://myblogactivation:cyKjAufKOSA9lLGW@cooking.bgsxm.mongodb.net/test?retryWrites=true&w=majority&appName=cooking';
 const PORT = process.env.PORT || 3000; // Використовуємо process.env.PORT для Heroku
 
+const corsOptions = {
+  origin: 'http://localhost:3000', // або ваш фронтенд домен
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, // Якщо ви працюєте з cookie або автентифікацією
+};
+
+
 const app = express();
 
 // Проксі для API запитів
@@ -25,7 +33,7 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.static(path.join('client', 'dist')));
 
 // Налаштування CORS
-app.use(cors());
+app.use(cors(corsOptions));
 
 // Middleware для обробки JSON у вхідних запитах
 app.use(express.json({ limit: '50mb' })); // Обмеження, якщо потрібно
